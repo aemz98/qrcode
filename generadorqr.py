6b282154f1cfeeeae13584e9667582304cddda5b
andres <aemz98@gmail.com>
2022-09-23 11:24:15 +0200

    se cambio el color y el fondo, se agrego un borde

--- generadorqr.py
+++ generadorqr.py
@@ -2,6 +2,16 @@ import qrcode
 
 data = 'Prueba de QR'
 
-img = qrcode.make(data)
+# img = qrcode.make(data)
 
-img.save('C:/Users/aemz9/Pictures/imagenesqr/primero.png')
+# img.save('C:/Users/aemz9/Pictures/imagenesqr/primero.png')
+
+qr = qrcode.QRCode(version = 1, box_size=10, border=5)
+
+qr.add_data(data)
+
+qr.make(fit=True)
+
+img = qr.make_image(fill_color = 'red', back_color = 'black')
+
+img.save('C:/Users/aemz9/Pictures/imagenesqr/segundo.png')
